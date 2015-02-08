fd11c23fd220272eab504d14d134c6dc46a7b7a4
DanielCatz <daniel.caterson@gmail.com>
2015-02-07 23:35:13 -0500

    Sliding info pane
    gradle entry added
    new xml layout added
    maps calls new layout

--- app/src/main/java/delta/soen390/mapsters/MapsActivity.java
+++ app/src/main/java/delta/soen390/mapsters/MapsActivity.java
@@ -15,7 +15,7 @@ public class MapsActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.slide);
         setUpMapIfNeeded();
     }
 
