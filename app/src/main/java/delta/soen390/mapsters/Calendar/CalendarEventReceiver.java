a653f3b4f94315fb6dcf131d95bf1dc244d8b23d
Felicia Santoro-Petti <fsantoropetti@gmail.com>
2015-02-21 20:33:52 -0500

    MAP-31
    Added necessary attributes in the AndroidManifest.xml to enable the use of push notifications.
    Created CalendarEventReceiver class where it is responsible for handling the intent for push notifications.

---
+++ app/src/main/java/delta/soen390/mapsters/Calendar/CalendarEventReceiver.java
@@ -0,0 +1,16 @@
+package delta.soen390.mapsters.Calendar;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Felicia on 2015-02-21.
+ */
+public class CalendarEventReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
