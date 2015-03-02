47d7c831f64e289e222de322b3b4f9af026f144a
Mathieu St-Louis <stlouis_mathieu@hotmail.com>
2015-03-01 21:58:41 -0500

    MAP-134  GoogleUtil - LatLng Utils
    Created the GoogleUtils class
    - Implemented Maps ->Directions LatLng conversion
    - Implemented Directions -> Maps LatLng conversion

---
+++ app/src/main/java/delta/soen390/mapsters/Utils/GoogleUtils.java
@@ -0,0 +1,18 @@
+package delta.soen390.mapsters.Utils;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by Mathieu on 3/1/2015.
+ */
+public class GoogleUtils {
+    public static com.google.android.gms.maps.model.LatLng toMapsLatLng(com.google.maps.model.LatLng latLng)
+    {
+        return new com.google.android.gms.maps.model.LatLng(latLng.lat, latLng.lng);
+    }
+
+    public static com.google.maps.model.LatLng toDirectionsLatLng(com.google.android.gms.maps.model.LatLng latLng)
+    {
+        return new com.google.maps.model.LatLng(latLng.latitude,latLng.longitude);
+    }
+}
