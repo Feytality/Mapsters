11454cfb390edbaf2ddacd41a2b216dc7ed585d2
GeorgeValergas <gvalergas@gmail.com>
2015-03-01 21:49:17 -0500

    Modified TravelResponseInfo to have start and end points
    Reviewed by: Mathieu

--- app/src/main/java/delta/soen390/mapsters/Services/TravelResponseInfo.java
+++ app/src/main/java/delta/soen390/mapsters/Services/TravelResponseInfo.java
@@ -15,12 +15,23 @@ public class TravelResponseInfo {
     //start & arrival time in milliseconds
     private long startTime;
     private long arrivalTime;
+    private LatLng startPoint;
+    private LatLng destinationPoint;
     private ArrayList<TravelStep> travelSteps;
+
+
+    public TravelResponseInfo(long startTime, long arrivalTime, EncodedPolyline encodedPolyline, ArrayList<TravelStep> travelSteps) {
+        this.startTime = startTime;
+        this.arrivalTime = arrivalTime;
+        List<LatLng> decodedLinePoints = encodedPolyline.decodePath();
+        this.startPoint = decodedLinePoints.get(0);
+        this.destinationPoint = decodedLinePoints.get(decodedLinePoints.size() -1);
+        this.travelSteps = travelSteps;
+    }
 
     public long getStartTime() {
         return this.startTime;
     }
-
     public long getArrivalTime() {
         return this.arrivalTime;
     }
@@ -28,30 +39,32 @@ public class TravelResponseInfo {
     public ArrayList<TravelStep> getTravelSteps() {
         return  this.travelSteps;
     }
-
     public Iterator<TravelStep> getIterator()
     {
         return travelSteps.iterator();
     }
 
+    public LatLng getStartPoint() {
+        return startPoint;
+    }
+    public LatLng getDestinationPoint() {
+        return destinationPoint;
+    }
 
-    public class TravelStep {
+    private class TravelStep {
 
         public TravelStep(String stepName, EncodedPolyline encodedPolyLine) {
             this.stepName = stepName;
-            this.decodedLinePoints = encodedPolyLine.decodePath();
+            this.encodedPolyLine = encodedPolyLine;
         }
         private String stepName;
-        private List<LatLng> decodedLinePoints;
+        private EncodedPolyline encodedPolyLine;
 
         public String getStepName() {
             return this.stepName;
         }
-        public LatLng getStartPoint() {
-            return decodedLinePoints.get(0);
-        }
-        public LatLng getDestinationPoint() {
-            return decodedLinePoints.get(decodedLinePoints.size() -1);
+        public  EncodedPolyline getEncodedPolyLine() {
+            return this.encodedPolyLine;
         }
 
 
