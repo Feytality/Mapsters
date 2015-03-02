42520b4634e51a18a9e0e4a79326f8672c9932ea
Mathieu St-Louis <stlouis_mathieu@hotmail.com>
2015-03-01 22:01:33 -0500

    MAP-134 Renamed GoogleUtils
    Resolved a naming conflict with a pre-existing class by renaming
    GoogleUtils to GoogleMapstersUtils

--- app/src/main/java/delta/soen390/mapsters/Utils/GoogleMapstersUtils.java
+++ app/src/main/java/delta/soen390/mapsters/Utils/GoogleMapstersUtils.java
@@ -5,7 +5,7 @@ import com.google.android.gms.maps.model.LatLng;
 /**
  * Created by Mathieu on 3/1/2015.
  */
-public class GoogleUtils {
+public class GoogleMapstersUtils {
     public static com.google.android.gms.maps.model.LatLng toMapsLatLng(com.google.maps.model.LatLng latLng)
     {
         return new com.google.android.gms.maps.model.LatLng(latLng.lat, latLng.lng);
