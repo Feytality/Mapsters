a06b0f73bae5e11fe81ac53f44fb177463eb1d0f
Patrick Nguyen <superpatricko@gmail.com>
2015-03-08 21:05:38 -0400

    Added tests for DirectoryActivity, LaunchActivity and SlideFragment

---
+++ app/src/androidTest/java/delta/soen390/mapsters/ActivitiesTest/SlideFragmentTest.java
@@ -0,0 +1,33 @@
+package delta.soen390.mapsters.ActivitiesTest;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+
+import delta.soen390.mapsters.Activities.SlideFragment;
+
+/**
+ * Created by Patrick on 15-03-08.
+ */
+public class SlideFragmentTest extends ActivityInstrumentationTestCase2 {
+
+    private SlideFragment mFragment;
+    private View mLaunch;
+
+    public SlideFragmentTest() {
+        super(SlideFragment.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        setActivityInitialTouchMode(false);
+    }
+
+    public void testPreConditions() {
+        assertNull(mFragment);
+    }
+    public void testLaunchNullValue() {
+        assertNull(mLaunch);
+    }
+
+}
